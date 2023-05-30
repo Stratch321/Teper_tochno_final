75c7b3bf3e17c2878f27215543c4153e9a471636
Igor <khmelinsky.igor@gmail.com>
2023-05-30 18:59:58 +0300

    navigation via bar

--- app/src/main/java/com/example/myapplication/main_app.java
+++ app/src/main/java/com/example/myapplication/main_app.java
@@ -1,14 +1,38 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.example.myapplication.databinding.ActivityMainAppBinding;
+import com.example.myapplication.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class main_app extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_app);
+            BottomNavigationView vw = findViewById(R.id.nav_bar);
+            vw.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Navigation.findNavController(main_app.this, R.id.fragmentContainerView).navigate(item.getItemId());
+                    return false;
+                }
+            });
+
+        };
     }
-}
